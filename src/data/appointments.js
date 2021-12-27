3b223487e0ba618e96e6fe18ff19649a7341ae14
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2021-12-27 20:12:03 +0700

    calender view

---
+++ src/data/appointments.js
@@ -0,0 +1,10 @@
+export const appointments = [
+    {
+      title: 'Website Re-Design Plan',
+      startDate: new Date(2021, 12, 26, 9, 35),
+      endDate: new Date(2021, 12, 28, 11, 30),
+      id: 0,
+      location: 'Room 1',
+    }
+  ];
+  
